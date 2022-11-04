707bfb0b04fe8fb146b83b3f81ed36abfcb4e343
thayscasado <thayscasado@hotmail.com>
2022-11-04 03:45:36 -0700

    adds cors setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,4 @@
+import cors from '@fastify/cors';
 import { PrismaClient } from '@prisma/client';
 import Fastify from 'fastify';
 
@@ -8,12 +9,16 @@ const prisma = new PrismaClient({
 async function bootstrap() {
   const fastify = Fastify({ logger: true }); // set logger to true to enable logging
 
+  await fastify.register(cors, {
+    origin: true, // allow all origins; production should be domain name
+  });
+
   fastify.get('/pools/count', async () => {
     const count = await prisma.pool.count();
     return { count };
   });
 
-  await fastify.listen({ port: 3333 });
+  await fastify.listen({ port: 3333, host: '0.0.0.0' });
 }
 
 bootstrap();
